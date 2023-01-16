cf20b14120c84f0a0ecadad5cafe77de7ff6a06c
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-01-15 22:44:14 -0600

    feat: add interaction creation event handler

---
+++ src/events/guild/InteractionCreateEvent.ts
@@ -0,0 +1,45 @@
+import {
+  Events,
+  GuildChannel,
+  Interaction,
+  PermissionsBitField
+} from 'discord.js';
+import Bot from '../../Bot';
+import GuildEvent from '../../interfaces/GuildEvent';
+
+const callback = (bot: Bot, interaction: Interaction) => {
+  const channel = interaction.channel;
+  const permissions = interaction.memberPermissions;
+
+  if (!interaction.isChatInputCommand()) return;
+  if (!(channel instanceof GuildChannel)) return;
+  if (!(permissions instanceof PermissionsBitField)) return;
+
+  bot
+    .findCommand(interaction.commandName)
+    .then((command) => {
+      bot.logger.debug('Handling command...');
+
+      if (!command.calledInPermittedChannel(channel.name)) {
+        interaction.reply({
+          content: 'Not permitted to use this command in this channel.',
+          ephemeral: true
+        });
+      } else if (!command.calledWithNecessaryPermissions(permissions)) {
+        interaction.reply({
+          content: 'Not permitted to use this command with your permissions.',
+          ephemeral: true
+        });
+      } else {
+        command.handle(bot, interaction);
+      }
+    })
+    .catch(bot.logger.error);
+};
+
+const InteractionCreateEvent: GuildEvent<Interaction> = {
+  name: Events.InteractionCreate,
+  callback
+};
+
+export default InteractionCreateEvent;
