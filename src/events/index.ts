7a643bdcc3a7faf91a6294c131bf278a97b921a8
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-01-15 22:44:41 -0600

    fix: enable interactionCreate event

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,6 +1,10 @@
 import CronEvent from '../interfaces/CronEvent';
 import GuildEvent from '../interfaces/GuildEvent';
 import ClientReadyEvent from './guild/ClientReadyEvent';
+import InteractionCreateEvent from './guild/InteractionCreateEvent';
 
 export const CronEvents: CronEvent[] = [];
-export const GuildEvents: GuildEvent<any>[] = [ClientReadyEvent];
+export const GuildEvents: GuildEvent<any>[] = [
+  ClientReadyEvent,
+  InteractionCreateEvent
+];
