247c5c278c3c48bcba1ffda8873bf2f1521dd57c
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-01-18 00:35:25 -0600

    feat: add user and category options

--- src/commands/VoteCommand.ts
+++ src/commands/VoteCommand.ts
@@ -3,9 +3,12 @@ import Bot from '../Bot';
 import Command from '../classes/Command';
 
 const callback = (_bot: Bot, interaction: ChatInputCommandInteraction) => {
+  const voter = interaction.user;
+  const target = interaction.options.getUser('user');
+  const category = interaction.options.getString('category');
+
   interaction.reply({
-    content: 'Received `vote` command.',
-    ephemeral: true
+    content: `${voter} voted ${target} as this week's ${category}-of-the-week!`
   });
 };
 
@@ -13,6 +16,22 @@ export default new Command(
   new SlashCommandBuilder()
     .setName('vote')
     .setDescription("Vote for this week's Chump/Champ of the Week.")
+    .addUserOption((option) =>
+      option
+        .setName('user')
+        .setDescription('Who are you voting for?')
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName('category')
+        .setDescription('What are you voting them for?')
+        .addChoices(
+          { name: 'champ', value: 'champ' },
+          { name: 'chump', value: 'chump' }
+        )
+        .setRequired(true)
+    )
     .toJSON(),
   callback
 );
