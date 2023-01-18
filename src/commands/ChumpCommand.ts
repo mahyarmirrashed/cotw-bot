4784f1c3f5399414eb702a7891ce4bec41793cab
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-01-18 00:24:33 -0600

    feat: add descriptive interaction reply

--- src/commands/ChumpCommand.ts
+++ src/commands/ChumpCommand.ts
@@ -1,11 +1,15 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 import Bot from '../Bot';
 import Command from '../classes/Command';
+import { REASON_MAXIMUM_LENGTH, REASON_MINIMUM_LENGTH } from './constants';
 
 const callback = (_bot: Bot, interaction: ChatInputCommandInteraction) => {
+  const nominator = interaction.user;
+  const nominee = interaction.options.getUser('user');
+  const reason = interaction.options.getString('reason');
+
   interaction.reply({
-    content: 'Received `chump` command.',
-    ephemeral: true
+    content: `${nominator} chumped ${nominee} for ${reason}.`
   });
 };
 
@@ -13,6 +17,20 @@ export default new Command(
   new SlashCommandBuilder()
     .setName('chump')
     .setDescription('Nominate a Chump of the Week.')
+    .addUserOption((option) =>
+      option
+        .setName('user')
+        .setDescription('Who do you want to nominate?')
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName('reason')
+        .setDescription('This person is chumpy for...')
+        .setMinLength(REASON_MINIMUM_LENGTH)
+        .setMaxLength(REASON_MAXIMUM_LENGTH)
+        .setRequired(true)
+    )
     .toJSON(),
   callback
 );
