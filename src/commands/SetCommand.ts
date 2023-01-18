a81afc5c80e5e32fb2e080a7ace6b3d5aea82b71
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-01-18 01:06:53 -0600

    feat: add logic for setting cotw channel

--- src/commands/SetCommand.ts
+++ src/commands/SetCommand.ts
@@ -1,18 +1,43 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import {
+  ChannelType,
+  ChatInputCommandInteraction,
+  PermissionFlagsBits,
+  SlashCommandBuilder
+} from 'discord.js';
 import Bot from '../Bot';
 import Command from '../classes/Command';
 
 const callback = (_bot: Bot, interaction: ChatInputCommandInteraction) => {
-  interaction.reply({
-    content: 'Received `set` command.',
-    ephemeral: true
-  });
+  const channel = interaction.channel;
+  const me = interaction.guild?.members.me;
+
+  if (channel === null) return;
+  if (me === null || me === undefined) return;
+
+  if (channel.type !== ChannelType.GuildText) {
+    interaction.reply({
+      content: 'Channel must be a text channel.',
+      ephemeral: true
+    });
+  } else if (
+    !channel.permissionsFor(me).has(PermissionFlagsBits.SendMessages)
+  ) {
+    interaction.reply({
+      content: 'Do not have permissions to send messages in this channel.',
+      ephemeral: true
+    });
+  } else {
+    interaction.reply({
+      content: `Set ${channel} as the COTW channel.`,
+      ephemeral: true
+    });
+  }
 };
 
 export default new Command(
   new SlashCommandBuilder()
     .setName('set')
-    .setDescription('Set the COTW channel.')
+    .setDescription('Set this channel as the COTW channel.')
     .toJSON(),
   callback
 );
