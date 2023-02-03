2befb46fa599fd060112d5e2dfc96f5759f87661
Mahyar Mirrashed <mah.mirr@gmail.com>
2023-02-03 00:11:33 -0600

    feat: add database functionality to voting command

--- src/commands/VoteCommand.ts
+++ src/commands/VoteCommand.ts
@@ -1,15 +1,120 @@
+import { NominationType, PrismaClient } from '@prisma/client';
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import moment from 'moment';
 import Bot from '../Bot';
 import Command from '../classes/Command';
 
-const handler = (_bot: Bot, interaction: ChatInputCommandInteraction) => {
+const checkCandidateExists = async (
+  prisma: PrismaClient,
+  serverId: string,
+  candidateId: string,
+  weekNumber: number,
+  nominationType: NominationType
+) => {
+  return prisma.candidate.findFirstOrThrow({
+    where: {
+      candidateId,
+      serverId,
+      weekNumber
+    },
+    include: {
+      eligibility: {
+        where: {
+          type: nominationType
+        }
+      }
+    }
+  });
+};
+
+const createVote = async (
+  prisma: PrismaClient,
+  serverId: string,
+  voterId: string,
+  weekNumber: number
+) => {
+  return prisma.vote.upsert({
+    where: {
+      serverId_voterId_weekNumber: {
+        serverId,
+        voterId,
+        weekNumber
+      }
+    },
+    create: {
+      server: {
+        connect: { id: serverId }
+      },
+      voterId,
+      weekNumber
+    },
+    update: {}
+  });
+};
+
+const updateVote = async (
+  prisma: PrismaClient,
+  serverId: string,
+  voterId: string,
+  weekNumber: number,
+  candidateId: string,
+  nominationType: NominationType
+) => {
+  return prisma.candidate
+    .findFirstOrThrow({
+      where: {
+        candidateId,
+        serverId,
+        weekNumber
+      }
+    })
+    .then((candidate) =>
+      prisma.vote.update({
+        where: {
+          serverId_voterId_weekNumber: {
+            serverId,
+            voterId,
+            weekNumber
+          }
+        },
+        data: {
+          [nominationType === NominationType.CHAMP ? 'champ' : 'chump']: {
+            connect: {
+              id: candidate.id
+            }
+          }
+        }
+      })
+    );
+};
+
+const handler = (bot: Bot, interaction: ChatInputCommandInteraction) => {
   const voter = interaction.user;
   const target = interaction.options.getUser('user', true);
-  const category = interaction.options.getString('category', true);
+  const type = <NominationType>interaction.options.getString('category', true);
 
-  interaction.reply({
-    content: `${voter} voted ${target} as this week's ${category}-of-the-week!`
-  });
+  const guild = interaction.guild;
+  const weekNumber = moment().subtract(1, 'weeks').isoWeek();
+
+  if (!guild) return;
+
+  checkCandidateExists(bot.prisma, guild.id, target.id, weekNumber, type)
+    .catch(() =>
+      interaction.reply({
+        content: `${voter} is not eligible to be voted as ${type}-of-the-week.`,
+        ephemeral: true
+      })
+    )
+    .then(() => createVote(bot.prisma, guild.id, voter.id, weekNumber))
+    .then(() =>
+      updateVote(bot.prisma, guild.id, voter.id, weekNumber, target.id, type)
+    )
+    .then(() =>
+      interaction.reply({
+        content: `${voter} voted ${target} as this week's ${type}-of-the-week!`
+      })
+    )
+    .catch(bot.logger.error);
 };
 
 export default new Command(
@@ -27,8 +132,8 @@ export default new Command(
         .setName('category')
         .setDescription('What are you voting them for?')
         .addChoices(
-          { name: 'champ', value: 'champ' },
-          { name: 'chump', value: 'chump' }
+          { name: 'champ', value: NominationType.CHAMP },
+          { name: 'chump', value: NominationType.CHUMP }
         )
         .setRequired(true)
     )
